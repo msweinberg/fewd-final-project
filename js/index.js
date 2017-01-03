57bcfd23fc33f0d98f07085b37f807a99e667850
msweinberg <maayan.weinberg@9-dots.org>
2017-01-03 15:59:03 -0800

    Sticky Generator & Brainstorm Board
    When you enter a # of lessons it populates the brainstorm board with
    that # of divs (this still needs to be styled).
    
    Sticky Generator clones sticky template when you click the cloning
    icon. Stickies are draggable.
    
    ISSUES - Something funky is happening when you clone the sticky more
    than once. There is a double clone and the draggable function is not
    working on the second sticky. Not sure what’s happening here.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
+
 $("#howmany").hide();
 
-	$( "#planner" ).click(function(){
+	$( "#planner" ).click(function(e){
+		e.preventDefault();
+		// debugger;
  	 $( "#planner" ).animate({ left: "175px" });
  	 console.log("clicked");
  	 $("#howmany").show();
@@ -10,5 +13,51 @@ $("#howmany").hide();
  	 // $("#howmany").toggle("clip");
  	});
 
+// Makes that sticky draggable
+// $( init );
+//  function init() {
+//   $('.sticky-template').draggable();
+// }
+
+// When you click a sticky it clones
+// When a sticky is cloned it is appended to the "sticky-generator"
+// The sticky becomes draggable and can be moved onto the "brainstorm-board"
+$('#clone-button').click(function(){
+	$('<p></p>').appendTo($('#sticky-generator'))
+	var newSticky = $('.sticky-template').clone();
+	newSticky.draggable();
+	newSticky.appendTo($('#sticky-generator'))
+	
+})
+
+// get the value of the input and populate the brainstorm board with that many divs
+$("#number-of-lessons").keyup(function() {
+    var value = $( this ).val();
+    $( "p" ).text( value );
+    console.log(value)
+    for (i = value; i > 0; i--) {
+    	console.log(i)
+    	$('#brainstorm-board').append('<div class="lesson"></div>')
+    }
+  })
+
+
+// Smooth Scrolling from https://css-tricks.com/snippets/jquery/smooth-scrolling/ 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
 })
+
+
---
+++ readme.md
@@ -0,0 +1,23 @@
+To-Do List
+
+##HTML Stuff
+- ~~Add an icon for cloning the sticky~~
+- ~~Add text input area for sticky~~
+- Add ability to change sticky color
+
+##JS Stuff
+- ~~Populate brainstorm-board w/ divs based on the # of lessons in the splash div~~
+- listen to the id of the form and submit on that id 
+- After submit,
+1. ~~Smooth scroll~~
+1. ~~get the value~~
+1. ~~populate the divs (use append)~~
+1. Adjust the CSS for the divs to fit the brainstorm board based on how many lessons
+- possibly grab the value in a global variable
+
+- When a sticky is cloned it is appended to the "sticky-generator"
+- The sticky becomes draggable and can be moved onto the "brainstorm-board"
+- When a sticky is moved to the "brainstorm-board" it is actually a clone that is dragged so the original stays in the sticky generator 
+
+##CS Stuff
+- Create div style for stickies
